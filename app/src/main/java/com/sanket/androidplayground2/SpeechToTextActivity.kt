e67e0cf2b39ea29f3b76f986c1f34f3dbd3dbc11
Sanket Vetkoli <vetkolisanket@gmail.com>
2020-01-10 12:59:45 +0530

    Added mic icon in STTA

--- app/src/main/java/com/sanket/androidplayground2/SpeechToTextActivity.kt
+++ app/src/main/java/com/sanket/androidplayground2/SpeechToTextActivity.kt
@@ -2,11 +2,20 @@ package com.sanket.androidplayground2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_speech_to_text.*
 
 class SpeechToTextActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_speech_to_text)
+        init()
     }
+
+    private fun init() {
+        ivMic.setOnClickListener {
+
+        }
+    }
+
 }
