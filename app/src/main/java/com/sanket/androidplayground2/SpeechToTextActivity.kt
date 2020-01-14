e54aa8790dbcd9d364236841f32273bc3776daed
Sanket Vetkoli <vetkolisanket@gmail.com>
2020-01-14 10:55:44 +0530

    Added speech to text in english language

--- app/src/main/java/com/sanket/androidplayground2/SpeechToTextActivity.kt
+++ app/src/main/java/com/sanket/androidplayground2/SpeechToTextActivity.kt
@@ -1,12 +1,19 @@
 package com.sanket.androidplayground2
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_speech_to_text.*
+import java.util.*
 
 class SpeechToTextActivity : AppCompatActivity() {
+
+    private val REQUEST_CODE_SPEECH_INPUT: Int = 1001
 
     companion object {
         fun newIntent(context: Context) = Intent(context, SpeechToTextActivity::class.java)
@@ -20,7 +27,35 @@ class SpeechToTextActivity : AppCompatActivity() {
 
     private fun init() {
         ivMic.setOnClickListener {
+            promptSpeechInput()
+        }
+    }
 
+    private fun promptSpeechInput() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.apply {
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.speech_prompt))
+        }
+        try {
+            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
+        } catch (a: ActivityNotFoundException) {
+            Toast.makeText(this, R.string.speech_not_supported, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQUEST_CODE_SPEECH_INPUT -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    val result =
+                        data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    Toast.makeText(this, result.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
