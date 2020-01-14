8fac64d2060d752ff53912b345cee53406713e8b
Sanket Vetkoli <vetkolisanket@gmail.com>
2020-01-14 10:41:23 +0530

    Opening STTA from MA

--- app/src/main/java/com/sanket/androidplayground2/MainActivity.kt
+++ app/src/main/java/com/sanket/androidplayground2/MainActivity.kt
@@ -2,12 +2,22 @@ package com.sanket.androidplayground2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    init()
   }
+
+  private fun init() {
+    btnSpeechToText.setOnClickListener {
+      startActivity(SpeechToTextActivity.newIntent(this))
+    }
+  }
+
 
 }
--- app/src/main/java/com/sanket/androidplayground2/SpeechToTextActivity.kt
+++ app/src/main/java/com/sanket/androidplayground2/SpeechToTextActivity.kt
@@ -1,10 +1,16 @@
 package com.sanket.androidplayground2
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_speech_to_text.*
 
 class SpeechToTextActivity : AppCompatActivity() {
+
+    companion object {
+        fun newIntent(context: Context) = Intent(context, SpeechToTextActivity::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
