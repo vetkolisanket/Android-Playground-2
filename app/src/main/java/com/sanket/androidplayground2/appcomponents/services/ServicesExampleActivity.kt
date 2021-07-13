6c8c81b6c07c31ecd79ab9ed21fdf1651ff2ee9d
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2021-07-13 15:16:50 +0530

    Added sevices example activity

---
+++ app/src/main/java/com/sanket/androidplayground2/appcomponents/services/ServicesExampleActivity.kt
@@ -0,0 +1,12 @@
+package com.sanket.androidplayground2.appcomponents.services
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sanket.androidplayground2.R
+
+class ServicesExampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_services_example)
+    }
+}
