2804ac959ecdd0e5e1d96645e1eb67f6ce370be4
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2021-08-15 20:19:50 +0530

    Added btn and coroutines activity

--- app/src/main/java/com/sanket/androidplayground2/MainActivity.kt
+++ app/src/main/java/com/sanket/androidplayground2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sanket.androidplayground2.appcomponents.AppComponentsActivity
 import com.sanket.androidplayground2.commons.utils.openActivity
+import com.sanket.androidplayground2.coroutines.CoroutinesActivity
 import com.sanket.androidplayground2.hilt.HiltActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -21,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     btnSearch.setOnClickListener { startActivity(SearchActivity.newIntent(this)) }
     btnAppComponents.setOnClickListener { startActivity(AppComponentsActivity.newIntent(this)) }
     btnHilt.setOnClickListener { openActivity<HiltActivity>() }
+    btnCoroutines.setOnClickListener { openActivity<CoroutinesActivity>() }
   }
 
 
---
+++ app/src/main/java/com/sanket/androidplayground2/coroutines/CoroutinesActivity.kt
@@ -0,0 +1,12 @@
+package com.sanket.androidplayground2.coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sanket.androidplayground2.R
+
+class CoroutinesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coroutines)
+    }
+}
