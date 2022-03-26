eaa7f42f7bac0b65d51bec7faa08647b7a1602ab
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-03-26 21:45:33 +0530

    Added activity for compose examples

--- app/src/main/java/com/sanket/androidplayground2/MainActivity.kt
+++ app/src/main/java/com/sanket/androidplayground2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sanket.androidplayground2.appcomponents.AppComponentsActivity
 import com.sanket.androidplayground2.commons.utils.openActivity
+import com.sanket.androidplayground2.compose.ComposeActivity
 import com.sanket.androidplayground2.coroutines.CoroutinesExamplesActivity
 import com.sanket.androidplayground2.hilt.HiltActivity
 import com.sanket.androidplayground2.widgets.WidgetsActivity
@@ -11,22 +12,23 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    init()
-  }
+        init()
+    }
 
-  private fun init() {
-    btnSpeechToText.setOnClickListener { startActivity(SpeechToTextActivity.newIntent(this)) }
-    btnSearch.setOnClickListener { startActivity(SearchActivity.newIntent(this)) }
-    btnAppComponents.setOnClickListener { startActivity(AppComponentsActivity.newIntent(this)) }
-    btnHilt.setOnClickListener { openActivity<HiltActivity>() }
-    btnCoroutines.setOnClickListener { openActivity<CoroutinesExamplesActivity>() }
-    btnWidget.setOnClickListener { openActivity<WidgetsActivity>() }
-    btnLibraries.setOnClickListener { openActivity<LibrariesActivity>() }
-  }
+    private fun init() {
+        btnSpeechToText.setOnClickListener { startActivity(SpeechToTextActivity.newIntent(this)) }
+        btnSearch.setOnClickListener { startActivity(SearchActivity.newIntent(this)) }
+        btnAppComponents.setOnClickListener { startActivity(AppComponentsActivity.newIntent(this)) }
+        btnHilt.setOnClickListener { openActivity<HiltActivity>() }
+        btnCoroutines.setOnClickListener { openActivity<CoroutinesExamplesActivity>() }
+        btnWidget.setOnClickListener { openActivity<WidgetsActivity>() }
+        btnLibraries.setOnClickListener { openActivity<LibrariesActivity>() }
+        btnCompose.setOnClickListener { openActivity<ComposeActivity>() }
+    }
 
 
 }
---
+++ app/src/main/java/com/sanket/androidplayground2/compose/ComposeActivity.kt
@@ -0,0 +1,12 @@
+package com.sanket.androidplayground2.compose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sanket.androidplayground2.R
+
+class ComposeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compose)
+    }
+}
