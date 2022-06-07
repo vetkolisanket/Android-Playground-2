4fa247ef574605e58350c462d46bdf23844f6741
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-06-07 11:02:36 +0530

    Added a few more lifecycle methods to AbstractLifecycleActivity

--- app/src/main/java/com/sanket/androidplayground2/appcomponents/activity/AbstractLifecycleActivity.kt
+++ app/src/main/java/com/sanket/androidplayground2/appcomponents/activity/AbstractLifecycleActivity.kt
@@ -2,10 +2,7 @@ package com.sanket.androidplayground2.appcomponents.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
-import android.widget.Toast
-import com.sanket.androidplayground2.R
 
 abstract class AbstractLifecycleActivity : AppCompatActivity() {
 
@@ -13,52 +10,66 @@ abstract class AbstractLifecycleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        logAndToast("onCreate")
+        log("onCreate")
     }
 
-    private fun logAndToast(functionName: String) {
-        val string = "$functionName called"
-        Log.d(getTag(), string)
-//        Toast.makeText(this, string, Toast.LENGTH_SHORT).show()
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        log("onPostCreate")
     }
 
     override fun onStart() {
         super.onStart()
-        logAndToast(this::onStart.name)
+        log(this::onStart.name)
     }
 
     override fun onResume() {
         super.onResume()
-        logAndToast(this::onResume.name)
+        log(this::onResume.name)
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+        log("onPostResume")
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        log("onResumeFragments")
     }
 
     override fun onPause() {
         super.onPause()
-        logAndToast(this::onPause.name)
+        log(this::onPause.name)
     }
 
     override fun onStop() {
         super.onStop()
-        logAndToast(this::onStop.name)
+        log(this::onStop.name)
     }
 
     override fun onRestart() {
         super.onRestart()
-        logAndToast(this::onRestart.name)
+        log(this::onRestart.name)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        logAndToast("onSaveInstanceState")
+        log("onSaveInstanceState")
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        logAndToast("onRestoreInstanceState")
+        log("onRestoreInstanceState")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        logAndToast(this::onDestroy.name)
+        log(this::onDestroy.name)
+    }
+
+    private fun log(functionName: String) {
+        val string = "$functionName called"
+        Log.d(getTag(), string)
     }
 }
