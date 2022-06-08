cfb4e212c8736470fdc2440dd4bfb2c9c3a0f2ef
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-06-08 21:39:57 +0530

    Added few lottie animations

--- app/src/main/java/com/sanket/androidplayground2/third_party/LottieActivity.kt
+++ app/src/main/java/com/sanket/androidplayground2/third_party/LottieActivity.kt
@@ -3,10 +3,28 @@ package com.sanket.androidplayground2.third_party
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sanket.androidplayground2.R
+import kotlinx.android.synthetic.main.activity_lottie.*
 
 class LottieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lottie)
+
+        initBtns()
+    }
+
+    private fun initBtns() {
+        btnBullseye.setOnClickListener {
+            lav.setAnimation(R.raw.bullseye)
+            lav.playAnimation()
+        }
+        btnFullScreen.setOnClickListener {
+            lav.setAnimation(R.raw.full_screen)
+            lav.playAnimation()
+        }
+        btnHamburgerArrow.setOnClickListener {
+            lav.setAnimation(R.raw.hamburger_arrow)
+            lav.playAnimation()
+        }
     }
 }
