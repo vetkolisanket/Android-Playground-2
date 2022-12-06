5de6627319ff2d257540a9534dc943e1cc4d10f7
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2022-12-06 19:16:33 +0530

    Converting ComposeActivity to use compose wip :P

--- app/src/main/java/com/sanket/androidplayground2/compose/ComposeActivity.kt
+++ app/src/main/java/com/sanket/androidplayground2/compose/ComposeActivity.kt
@@ -2,11 +2,14 @@ package com.sanket.androidplayground2.compose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import com.sanket.androidplayground2.R
 
 class ComposeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_compose)
+        setContent {
+
+        }
     }
 }
