0600cba88545067ac26000a09a8efbbf5d1dc98c
Sanket Vetkoli <sanket.vetkoli@agrostar.in>
2023-05-11 12:30:36 +0530

    Jetpack compose Wip

--- app/src/main/java/com/sanket/androidplayground2/compose/ComposeActivity.kt
+++ app/src/main/java/com/sanket/androidplayground2/compose/ComposeActivity.kt
@@ -3,13 +3,49 @@ package com.sanket.androidplayground2.compose
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sanket.androidplayground2.R
 
 class ComposeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(message: Message) {
+    Row (modifier = Modifier.padding(all = 8.dp)) {
+        Image(painter = painterResource(id = R.drawable.ic_mic),
+            contentDescription = "Picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = message.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = message.body)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(Message("Sanket", "Blah blah blah"))
 }
